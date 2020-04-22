9e00d0a934876046babd26ec629cb13b0e0eecb4
Jinghao Ye <jy3767a@student.american.edu>
2020-04-22 16:51:45 -0400

    update

--- virusdemo2.pl
+++ virusdemo2.pl
@@ -1,10 +1,15 @@
-tree(if_then_else('Do you have fever ?', 
+tree(if_then_else('Do you have fever ', 
                 /* level 1*/
                   if_then_else('It s been a week and it s getting worse', 
                   /* level 2*/
-                               if_then_else('has headache, joint pain, discomfort',
-                                            virus("fail to detect the virus"),
-                                            virus(cold)),
+                               if_then_else('In contact with a large crowd recently',
+                               /* level 3*/
+                                            if_then_else('has headache, joint pain, discomfort',
+                                            /* level 4*/
+                                                            virus(test1),
+                                                            virus("fail to detect")),
+                                            
+                                            virus("fail to detect")),
 
                                if_then_else(' The temperature rises to 39 degrees in one or two days',
                                             virus(flu),
